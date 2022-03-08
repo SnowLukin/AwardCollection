cf37f28e4f4a8a135d9e223fe48537815873d80d
Snow Lukin <everyoursden@gmail.com>
2022-03-08 23:15:48 +0300

    Initial Commit

---
+++ AwardCollection/AwardCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardCollectionApp.swift
+//  AwardCollection
+//
+//  Created by Snow Lukin on 08.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AwardCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
